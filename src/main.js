c242968527fbb30a4d5ab95f6b89e4496701ee25
wangh <wangh@shanshuiyun.cn>
2017-11-24 18:36:58 +0800

    create

---
+++ src/main.js
@@ -0,0 +1,58 @@
+import Vue from 'vue';
+import iView from 'iview';
+import VueRouter from 'vue-router';
+import Routers from './router';
+import Vuex from 'vuex';
+import Util from './libs/util';
+import App from './app.vue';
+import 'iview/dist/styles/iview.css';
+
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+Vue.use(iView);
+
+
+
+// 路由配置
+const RouterConfig = {
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    iView.LoadingBar.start();
+    Util.title(to.meta.title);
+    next();
+});
+
+router.afterEach(() => {
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
+});
+
+
+const store = new Vuex.Store({
+    state: {
+
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+});
+
+
+new Vue({
+    el: '#app',
+    router: router,
+    store: store,
+    render: h => h(App)
+});
---
+++ src/views/index.vue
@@ -0,0 +1,61 @@
+<style scoped>
+    .index {
+        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        text-align: center;
+    }
+    .index .ivu-row-flex {
+        height: 100%;
+    }
+    #index_pc_bj{width:100%;height:100%;background-size:cover;overflow: hidden;background-image: url(../images/login.jpg);background-position:center center;box-shadow: 0 0px 3px rgba(0,0,0,.5);margin-top:5px;text-align: center;}
+    /*具体内容*/
+    .wrap_conter ul{position:relative;width:300px;/*border:5px solid rgba(255,255,255,0.3);*/border-radius:5px;box-shadow: 0 0px 5px rgba(0,0,0,.2); margin-top: 10%; margin-left: 40%; text-align:center;}
+    .wrap_conter li{text-align:center;color:#fff;font-size:12px;line-height:30px; padding:0 25px 0 25px;width: 100%;}
+    .wrap_conter li h2{color:#FFFFFF;font-size:20px;line-height:40px; display:block; text-align:center; padding:20px 0 5px 0}
+    .pc-hign{height:75px;display: inline-table;}
+    .wrap_conter li dl{width:100%;}
+</style>
+<template>
+    <div class="index">
+        <div id="index_pc_bj">
+            <Form ref="formLogin">
+                <div class="wrap_conter">
+                    <ul>
+                        <li><h2>用户登录</h2></li>
+                        <li>
+                            <dl>
+                                <FormItem prop="loginName" >
+                                    <Input  type="text" placeholder="登录名" >
+                                        <Icon type="ios-person-outline" slot="prepend" ></Icon>
+                                    </Input>
+                                </FormItem>
+                                <FormItem prop="password">
+                                    <Input type="password" placeholder="密码" >
+                                    <Icon type="ios-locked-outline" slot="prepend"></Icon></Input>
+                                </FormItem>
+                                <FormItem>
+                                    <Button type="primary" @click="" style="width: 250px">登录</Button>
+                                </FormItem>
+                            </dl>
+                        </li>
+                    </ul>
+                </div>
+            </Form>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+
+            }
+        },
+        methods: {
+            
+        }
+    };
+</script>
