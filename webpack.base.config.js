cc360df6dd460053653ff385a88f6947a3674f16
wangh <wangh@shanshuiyun.cn>
2018-01-05 15:31:17 +0800

    ...

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -55,7 +55,7 @@ module.exports = {
             },
 
             {
-                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf|ico)\??.*$/,
                 loader: 'url-loader?limit=1024'
             },
             {
